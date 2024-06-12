18aae6292f578d58072720c693b03ff077e246b5
Kaisei Yoneyama <97094702+mikrogeophagus@users.noreply.github.com>
2024-06-12 21:15:54 +0900

    ファビコンを設定

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,6 +15,12 @@ const Layout: React.FunctionComponent<Props> = ({
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link
+        rel="icon"
+        href="/images/favicon.svg"
+        type="image/svg+xml"
+        sizes="any"
+      />
     </Head>
 
     <header className="px-5 ">
